76708c1c7b2b7d6e95099b2a00f183163ee1e219
Kari230996 <claro.claire1996@gmail.com>
2023-09-16 21:39:54 +0300

    added homework

---
+++ task_4.py
@@ -0,0 +1,14 @@
+'''
+✔ Создайте функцию генератор чисел Фибоначчи
+'''
+
+def fibonacci():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+fibonacci_generator = fibonacci()
+
+for _ in range(10):
+    print(next(fibonacci_generator))
